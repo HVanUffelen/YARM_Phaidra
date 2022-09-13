3d8af4830374210871838082d8621ba065eb7cc9
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-09-13 08:58:54 +0200

    add phaidraLibraries.blade.php

---
+++ src/views/DLBTStats/phaidraLibraries.blade.php
@@ -0,0 +1,11 @@
+<h2>Libraries</h2>
+<p><span style="font-weight: bold; ">Translations and Adaptations:</span></p>
+<ul>
+    <li>Total: {{$primary}}</li>
+    <li>Different Languages: {{$primaryLang}}</li>
+</ul>
+<p><span style="font-weight: bold; ">Reception Documents: </span></p>
+<ul>
+    <li>Total: {{$secondary}}</li>
+    <li>Different Languages: {{$secondaryLang}}</li>
+</ul>
